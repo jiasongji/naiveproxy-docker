b3695d86bb2fffb700724adb62e69cb3031d7ac8
松記 <48777154+jiasongji@users.noreply.github.com>
2024-02-21 09:05:30 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -225,7 +225,7 @@ echo ' |_| \_\__,_|\__, | |_| \_|\__,_| |_|   \_/ \___| '
 echo '             |___/                                '
 
 # ------------vars-----------、
-gitRowUrl="https://raw.githubusercontent.com/RayWangQvQ/naiveproxy-docker/main"
+gitRowUrl="https://raw.githubusercontent.com/jiasongji/naiveproxy-docker/main"
 
 host=""
 
